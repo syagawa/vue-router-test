fb4c20ae52232aee0d95f7d74eea1f5a3075c7eb
yagawa <yagawa@qzc.co.jp>
2018-05-29 13:57:57 +0900

    Step 3 nested root

--- index.js
+++ index.js
@@ -4,7 +4,12 @@ const Foo = { template: '<div>foo</div>' };
 const Bar = { template: '<div>bar</div>' };
 
 const User = {
-  template: '<div>User {{ $route.params.id }}</div>',
+  template: `<div class="user">
+              <h2>User {{ $route.params.id }}</h2>
+              <router-link to="/user/11/profile">Go to profile</router-link>
+              <router-link to="/user/11/posts">Go to posts</router-link>
+              <router-view></router-view>
+            </div>`,
   // watch: {
   //   '$route' (to, from){
   //     console.info(to, from);
@@ -16,10 +21,33 @@ const User = {
   }
 };
 
+const UserProfile = {
+  template: `<div>User Profile {{ $route.params.id }}</div>`
+};
+
+const UserPosts = {
+  template: `<div>User Posts</div>`
+};
+
+
 const routes = [
   {path: '/foo', component: Foo },
   {path: '/bar', component: Bar },
-  {path: '/user/:id', component: User}
+  {
+    path: '/user/:id',
+    component: User,
+    children: [
+      {
+        path: 'profile',
+        component: UserProfile
+      },
+      {
+        path: 'posts',
+        component: UserPosts
+      }
+
+    ]
+  }
 ];
 
 const router = new VueRouter({

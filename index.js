8e9f45960c0fe72bfce6e14417f66809f8a7a771
yagawa <yagawa@qzc.co.jp>
2018-05-29 13:10:15 +0900

    Step 2 dynamic root matching

--- index.js
+++ index.js
@@ -3,9 +3,14 @@
 const Foo = { template: '<div>foo</div>' };
 const Bar = { template: '<div>bar</div>' };
 
+const User = {
+  template: '<div>User {{ $route.params.id }}</div>'
+};
+
 const routes = [
   {path: '/foo', component: Foo },
-  {path: '/bar', component: Bar }
+  {path: '/bar', component: Bar },
+  {path: '/user/:id', component: User}
 ];
 
 const router = new VueRouter({

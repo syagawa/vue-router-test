24de9c536241af82fa105a7638a4b612802711b0
yagawa <yagawa@qzc.co.jp>
2018-05-29 13:22:36 +0900

    Step 2 watch parameter

--- index.js
+++ index.js
@@ -4,7 +4,16 @@ const Foo = { template: '<div>foo</div>' };
 const Bar = { template: '<div>bar</div>' };
 
 const User = {
-  template: '<div>User {{ $route.params.id }}</div>'
+  template: '<div>User {{ $route.params.id }}</div>',
+  // watch: {
+  //   '$route' (to, from){
+  //     console.info(to, from);
+  //   }
+  // },
+  beforeRouteUpdate(to, from, next){
+    console.info(to, from, next);
+    next();
+  }
 };
 
 const routes = [
